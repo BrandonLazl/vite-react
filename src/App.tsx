5c02bbbc66dc3c16fffb57cb5793236cf17c77f9
Brandon <brandonlazl10@gmail.com>
2026-01-30 21:46:44 -0600

    import productos

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import Usuarios from './Usuarios'
+import Productos from './Productos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,17 +12,24 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <section style={{ marginTop: '2rem' }}>
         <h2>Usuarios</h2>
         <Usuarios />
+      </section>
+
+      <section style={{ marginTop: '2rem' }}>
+        <h2>Productos</h2>
+        <Productos />
       </section>
 
       <div className="card">
